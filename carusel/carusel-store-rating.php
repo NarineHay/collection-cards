7d5a982623a05a6999d3f82aebe7bcd8efc6a028
Tigran <1993tiko@gmail.com>
2020-09-10 17:08:59 +0400

    store-rating

--- carusel/carusel-store-rating.php
+++ carusel/carusel-store-rating.php
@@ -1,95 +1,38 @@
  <!-- ------------------section store rating--------------- -->
+ <?php
+ $sql = "SELECT * FROM store_rating";
+ $res=mysqli_query($con, $sql);
+ $tox = mysqli_fetch_assoc($res);
+ ?>
 <section id="clients" class="wow fadeInUp">
-<h2 class="text-center text-uppercase">store rating</h2>
+<h2 class="text-center text-uppercase"><?php echo $tox['title']?></h2>
 
     <div class="container">
         <div class="row row-collection owl-carousel clients-carousel">
-            <div class="  row-d-collector" >
-                <div class="autor-img1 text-center"><img src="images/e.png" class="logo11"></div>
-                <div class="collector-sec-rating text-center">
-                    <div class="">
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                    </div>
-                    <p class="collector-sec-author-name" >Ebay</p>
-                    <p class="collector-sec-country" >Country</p>
-                </div>
-                <div class="collector-sec-text text-justify">
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium deserunt at bland exercitationem? Cumque, recusandae sed aut cupiditate ipsum quasi.
-                </div>
-            </div>
-            <div class="row-d-collector" >
-                <div class="autor-img text-center"><img src="" class="logo22"></div>
-                <div class="collector-sec-rating text-center">
-                    <div class="">
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                    </div>
-                    <p class="collector-sec-author-name" >Aliexpress</p>
-                    <p class="collector-sec-country" >Country</p>
-                </div>
-                <div class="collector-sec-text text-justify">
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium deserunt at bland exercitationem? Cumque, recusandae sed aut cupiditate ipsum quasi.
-                </div>
-            </div>
-            <div class="row-d-collector" >
-                <div class="autor-img text-center">Store logo</div>
-                <div class="collector-sec-rating text-center">
-                    <div class="">
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                    </div>
-                    <p class="collector-sec-author-name" >Store name</p>
-                    <p class="collector-sec-country" >Country</p>
-                </div>
-                <div class="collector-sec-text text-justify">
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium deserunt at bland exercitationem? Cumque, recusandae sed aut cupiditate ipsum quasi.
-                </div>
-            </div>
-            <div class="row-d-collector" >
-                <div class="autor-img text-center">Store logo</div>
-                <div class="collector-sec-rating text-center">
-                    <div class="">
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                    </div>
-                    <p class="collector-sec-author-name" >Store name</p>
-                    <p class="collector-sec-country" >Country</p>
-                </div>
-                <div class="collector-sec-text text-justify">
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium deserunt at bland exercitationem? Cumque, recusandae sed aut cupiditate ipsum quasi.
-                </div>
-            </div>
-            
-            <div class="row-d-collector" >
-                <div class="autor-img text-center">Store logo</div>
-                <div class="collector-sec-rating text-center">
-                    <div class="">
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                        <span class="star1"><i class="fa fa-star-o"></i></span>
-                    </div>
-                    <p class="collector-sec-author-name" >Store name</p>
-                    <p class="collector-sec-country" >Country</p>
-                </div>
-                <div class="collector-sec-text text-justify">
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium deserunt at bland exercitationem? Cumque, recusandae sed aut cupiditate ipsum quasi.
-                </div>
-            </div>
+            <?php
+                $sql = "SELECT * FROM store_rating";
+                $res=mysqli_query($con, $sql);
+                while($row=mysqli_fetch_assoc($res)){
+                    echo "<div class='row-d-collector'>
+                                <div class='autor-img1 text-center'><img src='images/".$row['image']."' class='logo11'></div>
+                                <div class='collector-sec-rating text-center' style='margin-top: 60px'>
+                                    <div class=''>
+                                        <span class='star1'><i class='fa fa-star-o'></i></span>
+                                        <span class='star1'><i class='fa fa-star-o'></i></span>
+                                        <span class='star1'><i class='fa fa-star-o'></i></span>
+                                        <span class='star1'><i class='fa fa-star-o'></i></span>
+                                        <span class='star1'><i class='fa fa-star-o'></i></span>
+                                    </div>
+                                    <p class='collector-sec-author-name' >".$row['intotitle1']."</p>
+                                    <p class='collector-sec-country'>".$row['intotitle2']."</p>
+                                </div>
+                                <div class='collector-sec-text text-justify'>
+                                   <p>".$row['text']."</p> 
+                                </div>
+                            </div>";
+                }
+            ?>
+
         </div>
     </div>
    
