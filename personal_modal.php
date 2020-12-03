fadecdd9ad675c4e4a2bbee62c8b489428ad0056
Tigran <1993tiko@gmail.com>
2020-12-03 16:44:24 +0400

    profile-page news

--- personal_modal.php
+++ personal_modal.php
@@ -17,4 +17,21 @@
         }
       }
   }
+  if(isset($_POST['delite_btn-p'])){
+      $id = mysqli_real_escape_string($con, $_POST['delite_btn']);
+      $sql = "SELECT * FROM `personal_name_checklist` WHERE id=$id";
+      $query = mysqli_query($con, $sql);
+      $tox = mysqli_fetch_assoc($query);
+      $img = $tox['image'];
+      $del = "DELETE FROM `personal_name_checklist` WHERE id=$id";
+      $res = mysqli_query($con, $del);
+      if($res){
+        unlink('img/'.$img.'');
+        $del = "DELETE FROM `personal_checklist` WHERE cid=$id";
+        $res = mysqli_query($con, $del);
+        if($res){
+          header('location:profile-page.php');
+        }
+      }
+  }
 ?>
